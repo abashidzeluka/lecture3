81c1e9b55c4b3a5d90b66685281353a582ab1dd7
abashidzeluka <abashidze2003@gmail.com>
2026-05-12 23:01:57 +0400

    Initial commit

---
+++ Assignment1/Assignment1/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName, userPass;
+            userName = "admin";
+            userPass = "1234";
+
+            string userNameInp, userPassInp;
+
+            Console.Write("Enter username: ");
+            userNameInp = Console.ReadLine();
+            Console.Write("Enter password: ");
+            userPassInp = Console.ReadLine();
+
+            if (userName != userNameInp)
+            {
+                Console.WriteLine("Access Denied!");
+            }
+            else if (userPass == userPassInp)
+            {
+                Console.WriteLine("Welcome!");
+            }
+            else
+            {
+                Console.WriteLine("Access Denied!");
+            }
+        }
+    }
+}
