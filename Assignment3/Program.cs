1790f60a3741e07579b8d7d70392d7fc429536f4
abashidzeluka <abashidze2003@gmail.com>
2026-05-13 00:19:21 +0400

    Initial commit

---
+++ Assignment3/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Assignment3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your age: ");
+            byte age = Byte.Parse(Console.ReadLine());
+
+            if(age <= 12)
+            {
+                Console.WriteLine("You are a child");
+            }
+            else if (age >= 13 && age <= 19)
+            {
+                Console.WriteLine("You are a teenager");
+            }
+            else if (age >= 20 && age <= 64)
+            {
+                Console.WriteLine("You are a adult");
+            }
+            else if (age >= 65)
+            {
+                Console.WriteLine("You are a pensioner");
+            }
+           
+        }
+    }
+}
